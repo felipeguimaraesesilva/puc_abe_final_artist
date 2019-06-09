eb61f62cf4003247ce1bd9be6f843ef0b3811a1b
felipeguimaraesesilva <felipeguimaraesesilva@gmail.com>
2019-06-09 14:53:25 -0300

    Versionamento

--- puc_abe_artist_api/src/main/java/com/guimaraes/controller/ArtistController.java
+++ puc_abe_artist_api/src/main/java/com/guimaraes/controller/ArtistController.java
@@ -6,17 +6,28 @@ import java.util.List;
 import org.jmusixmatch.MusixMatch;
 import org.jmusixmatch.MusixMatchException;
 import org.jmusixmatch.entity.track.Track;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guimaraes.constant.AppConstants;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class ArtistController {
 
 	private static final String RESOURCE_NAME = "/artist";
 	private static final String CONTEXT_RESOURCE = "/" + AppConstants.VERSION_V1 + RESOURCE_NAME;
+
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@GetMapping(CONTEXT_RESOURCE)
 	public List<Track> getArtistByFilters(@RequestParam("artistName") String artistName) {
